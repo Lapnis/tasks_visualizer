9b18ffa0a224363b45d4a263a2b4976a77a24208
Salomon Torres <nomolas1@gmail.com>
2017-11-17 15:22:27 -0300

    Add deployment configurations

--- tasks_visualizer/settings.py
+++ tasks_visualizer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3k#1-5lr_09rbj+#0_mtz=-za#$6gtvfi&d@$g0y#+tu+%x-$u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tasksvisualizer.herokuapp.com']
 
 
 # Application definition
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=50)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
