16e259b8143e1bb0e8baa5a54ebfabb446a2d753
elbraulio <brauliop.3@gmail.com>
2017-11-24 13:03:57 -0300

    fix conflicts

--- tasks_visualizer/settings.py
+++ tasks_visualizer/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'week_scheduler/../static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_assets'),
+    os.path.join(BASE_DIR, 'week_scheduler/../static_assets'),
 )
