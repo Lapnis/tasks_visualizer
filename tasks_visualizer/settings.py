ec4cc34b3f899f19671418c64e361f5fdabdc009
Salomon Torres <nomolas1@gmail.com>
2017-11-24 13:18:45 -0300

    Add whitenoise for static files

--- tasks_visualizer/settings.py
+++ tasks_visualizer/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,5 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'week_scheduler/../static_assets'),
+    os.path.join(BASE_DIR, 'static_assets'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
