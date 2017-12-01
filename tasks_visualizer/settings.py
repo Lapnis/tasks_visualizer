f3e916d4b73509c232dab005e6bdf873e1ae83ab
Nicolás Bravo <nicolasbravo94@gmail.com>
2017-11-30 22:06:36 -0300

    help commit

--- tasks_visualizer/settings.py
+++ tasks_visualizer/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# i Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
